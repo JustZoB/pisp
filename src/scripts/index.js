501cfd2f7fe4b32210008d05cecc58d0e1360b17
Dmitriy Krasnoperov <dmitriy.krasnoperov@cimpleo.com>
2020-12-15 17:05:52 +0600

    Add safari check

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -6,3 +6,10 @@ import './modules/AboutBlock.js';
 import './modules/InsideBlock.js';
 import './modules/AdvantageBlock.js';
 import './modules/Game.js';
+
+const body = document.querySelector("body");
+const is_safari = /^((?!chrome|android).)*safari/i.test(navigator.userAgent);
+
+if (is_safari) {
+  body.classList.add("safari");
+}
